2b16237bf27dab6c81f706fcee7e3b8d5c2ecf9a
manuelnsosa <manuelsosa84@gmail.com>
2022-06-03 18:52:10 -0300

    🎉 begin to-do list project

---
+++ README.md
@@ -0,0 +1,2 @@
+# afterclass-context-31190
+Tailwind: https://tailwindcss.com/docs/guides/create-react-app
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import NavBar from "./components/NavBar";
+import ToDoContainer from "./components/ToDoContainer";
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <ToDoContainer />
+    </>
+  );
+}
+
+export default App;
